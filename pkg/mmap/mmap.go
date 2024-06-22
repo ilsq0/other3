5e5f5e9e3cfacd494a8f55a615e96819ad732c8c
isq0@rr2 <isq0@rr2.xyz>
2024-06-22 18:14:56 +0800

    +Map

---
+++ pkg/mmap/mmap.go
@@ -0,0 +1,55 @@
+package mmap
+
+import "sync"
+
+type Map[K comparable, V any] struct {
+	mp map[K]V
+	mu sync.RWMutex
+}
+
+func (m *Map[K, V]) Get(k K) V {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	return m.mp[k]
+}
+
+func (m *Map[K, V]) Load(k K, v V) (V, bool) {
+	m.mu.RLock()
+	v0, ok := m.mp[k]
+	m.mu.RUnlock()
+	if !ok {
+		m.mu.Lock()
+		defer m.mu.Unlock()
+		if v0, ok = m.mp[k]; !ok {
+			m.mp[k] = v
+			return v, ok
+		}
+	}
+	return v0, ok
+}
+
+func (m *Map[K, V]) Put(k K, v V) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.mp[k] = v
+}
+
+func (m *Map[K, V]) Remove(k K) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	delete(m.mp, k)
+}
+
+func (m *Map[K, V]) Count() int {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	return len(m.mp)
+}
+
+func (m *Map[K, V]) Loop(f func(K, V)) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	for k, v := range m.mp {
+		f(k, v)
+	}
+}
