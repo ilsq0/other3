60fda4e3b4e917483109d68d2a1778b911f0997b
isq0@rr2 <isq0@rr2.xyz>
2024-06-23 09:40:37 +0800

    minheap

--- pkg/sort/sort.go
+++ pkg/sort/sort.go
@@ -1,8 +1,10 @@
 package sort
 
-type Comparator[T any] func(a, b T) bool
+import (
+	"container/heap"
+)
 
-func merge[T any](a, b []T, less Comparator[T]) []T {
+func merge[T any](a, b []T, less func(T, T) bool) []T {
 	final := []T{}
 	i := 0
 	j := 0
@@ -26,7 +28,7 @@ func merge[T any](a, b []T, less Comparator[T]) []T {
 	return final
 }
 
-func MergeSort[T any](items []T, less Comparator[T]) []T {
+func MergeSort[T any](items []T, less func(T, T) bool) []T {
 	if len(items) < 2 {
 		return items
 	}
@@ -35,7 +37,7 @@ func MergeSort[T any](items []T, less Comparator[T]) []T {
 	return merge(first, second, less)
 }
 
-func InsertionSort[T any](arr []T, less Comparator[T]) {
+func InsertionSort[T any](arr []T, less func(T, T) bool) {
 	l := len(arr)
 	//从第二个元素开始
 	for i := 1; i < l; i++ {
@@ -44,5 +46,48 @@ func InsertionSort[T any](arr []T, less Comparator[T]) {
 			arr[j-1], arr[j] = arr[j], arr[j-1]
 			j--
 		}
+	}
+}
+
+type MinHeap[T any] struct {
+	capacity, size int
+	items          []T
+	less           func(T, T) bool
+}
+
+func NewMinHeap[T any](capacity int, less func(T, T) bool) *MinHeap[T] {
+	h := &MinHeap[T]{
+		capacity: capacity,
+		items:    make([]T, 0, capacity),
+		less:     less,
+	}
+	heap.Init(h)
+	return h
+}
+
+func (h MinHeap[T]) Len() int           { return h.size }
+func (h MinHeap[T]) Less(i, j int) bool { return h.less(h.items[i], h.items[j]) }
+func (h MinHeap[T]) Swap(i, j int)      { h.items[i], h.items[j] = h.items[j], h.items[i] }
+
+func (h *MinHeap[T]) Push(x any) {
+	h.items = append(h.items, x.(T))
+	h.size++
+}
+
+func (h *MinHeap[T]) Pop() any {
+	item := h.items[h.size-1]
+	h.items = h.items[:h.size-1]
+	h.size--
+	return item
+}
+
+func (h *MinHeap[T]) TryPush(x T) {
+	if h.size < h.capacity {
+		heap.Push(h, x)
+	} else {
+		if h.less(h.items[0], x) {
+			h.items[0] = x
+			heap.Fix(h, 0)
+		} 
 	}
 }
