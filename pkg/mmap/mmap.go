62950c5875714fbfcc892c0e7b2857989d5c3d12
isq0@rr2 <isq0@rr2.xyz>
2024-06-23 11:36:44 +0800

    mmap mutex

--- pkg/mmap/mmap.go
+++ pkg/mmap/mmap.go
@@ -8,23 +8,23 @@ func NewMap[K comparable, V any]() *Map[K, V] {
 
 type Map[K comparable, V any] struct {
 	mp map[K]V
-	mu sync.RWMutex
+	sync.RWMutex
 }
 
 func (m *Map[K, V]) Get(k K) (V, bool) {
-	m.mu.RLock()
-	defer m.mu.RUnlock()
+	m.RLock()
+	defer m.RUnlock()
 	v, b := m.mp[k]
 	return v, b
 }
 
 func (m *Map[K, V]) Load(k K, f func() V) V {
-	m.mu.RLock()
+	m.RLock()
 	v0, ok := m.mp[k]
-	m.mu.RUnlock()
+	m.RUnlock()
 	if !ok {
-		m.mu.Lock()
-		defer m.mu.Unlock()
+		m.Lock()
+		defer m.Unlock()
 		if v0, ok = m.mp[k]; !ok {
 			v0 = f()
 			m.mp[k] = v0
@@ -34,32 +34,32 @@ func (m *Map[K, V]) Load(k K, f func() V) V {
 }
 
 func (m *Map[K, V]) Put(k K, v V) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
+	m.Lock()
+	defer m.Unlock()
 	m.mp[k] = v
 }
 
 func (m *Map[K, V]) Remove(k K) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
+	m.Lock()
+	defer m.Unlock()
 	delete(m.mp, k)
 }
 
 func (m *Map[K, V]) Count() int {
-	m.mu.RLock()
-	defer m.mu.RUnlock()
+	m.RLock()
+	defer m.RUnlock()
 	return len(m.mp)
 }
 
 func (m *Map[K, V]) Inner() map[K]V {
-	m.mu.RLock()
-	defer m.mu.RUnlock()
+	m.RLock()
+	defer m.RUnlock()
 	return m.mp
 }
 
 func (m *Map[K, V]) Loop(f func(K, V)) {
-	m.mu.RLock()
-	defer m.mu.RUnlock()
+	m.RLock()
+	defer m.RUnlock()
 	for k, v := range m.mp {
 		f(k, v)
 	}
